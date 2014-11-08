43b756ede519dc5afde6cc78818c9b492fce9887
weidenfreak <meike.wiemann@gmx.de>
2014-11-08 19:08:56 +0100

    add space

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -1,9 +1,11 @@
+# -*- encoding : utf-8 -*-
 class StickiesController < ApplicationController
   respond_to :html, :json
-  
+
   def index
   	respond_with Sticky.all
   end
+
   def create
   	respond_with Sticky.create(sticky_params)
   end
