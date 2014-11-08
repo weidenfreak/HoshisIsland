c0944cfa9a3cb7b94f545ac9725b493dce6f9759
weidenfreak <meike.wiemann@gmx.de>
2014-11-08 18:26:40 +0100

    rm deprecation warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'factory_girl_rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
