58953f48b901164bb717a728c2abe8660041fa67
weidenfreak <meike.wiemann@gmx.de>
2014-11-09 10:29:57 +0100

    try 20 meter radius

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -4,7 +4,7 @@ class StickiesController < ApplicationController
 
   def index
     if params[:latitude] && params[:longitude]
-      respond_with Sticky.near([params[:latitude], params[:longitude]], 0.05, :units => :km)
+      respond_with Sticky.near([params[:latitude], params[:longitude]], 0.020, :units => :km)
     else
       respond_with Sticky.all
     end
