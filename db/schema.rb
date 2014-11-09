37bca061b0a5996d4fdb79c9e173f0a8381cc20f
weidenfreak <meike.wiemann@gmx.de>
2014-11-09 04:23:58 +0100

    use decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108234600) do
+ActiveRecord::Schema.define(version: 20141109031208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20141108234600) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.float    "latitude"
-    t.float    "longitude"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
     t.string   "pattern"
     t.integer  "radius"
     t.text     "note"
